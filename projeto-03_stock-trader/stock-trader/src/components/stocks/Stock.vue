fb7cc7be3f591f1d2e95f7027b6a3c34a6c9f7a1
Pedro Lucas Luckow <pedrolucasluckow@hotmail.com>
2023-11-08 18:28:28 -0300

    projeto03 - stock-trader (comeco)

---
+++ projeto-03_stock-trader/stock-trader/src/components/stocks/Stock.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-flex class="pr-3 pb-3" xs12 md6 lg4> <!-- 12 colunas no celular, 6 colunas no tablet, 4 colunas em telas grandes -->
+        <v-card class="brown darken-3 white--text">
+            <v-card-title class="heaadline">
+                <strong>{{ stock.name }}<small>(Preço: {{ stock.price }})</small></strong>
+            </v-card-title>
+        </v-card>
+        <v-card>
+            <v-container fill-height>
+                <v-text-field label="Quantidade" type="number" v-model.number="quantity"></v-text-field>
+                <v-btn class="brown darken-3 white--text" @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)">Comprar</v-btn>
+            </v-container>
+        </v-card>
+    </v-flex>
+</template>
+
+<script>
+    export default{
+        props: ['stock'],
+        data(){
+            return{
+                quantity: 0
+            }
+        },
+        methods: {
+            buyStock(){
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                }
+
+                console.log(order)
+                this.quantity = 0
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ projeto-03_stock-trader/stock-trader/src/store/modules/portfolio.js
@@ -0,0 +1,50 @@
+export default{
+    state: {
+        funds: 10000, // saldo em conta
+        stocks: []
+    },
+    mutations: {
+        buyStock(state, {stockId, quantity, stockPrice}){ // (estado, {objeto que representa uma compra})
+            const record = state.stocks.find(element => element.id == stockId) // procurar uma ação, se element.id for igual a stockId significa que já existe uma ação do mesmo tipo
+            if(record){ // Se já tiver uma ação com o mesmo ID vai apenas adicionar quantidade
+                record.quantity += quantity
+            } else{ // adicionar uma nova ação
+                state.stocks.push({
+                    id: stockId,
+                    quantity: quantity
+                })
+            }
+            state.funds -= stockPrice * quantity // ajustar o saldo pra refletir a compra
+        },
+        sellStock(state, {stockId, quantity, stockPrice}){ // (estado, {ordem de venda})
+            const record = state.stocks.find(element => element.id == stockId)
+            if(record.quantity > quantity){ // a quantidade de ações que eu tenho é maior que a quantidade de ações que quero vender?
+                record.quantity -= quantity
+            } else {
+                state.stocks.splice(state.stocks.indexOf(record), 1) // vender tudo (removendo um elemento do array)
+            }
+            state.funds += stockPrice * quantity // ajustar o saldo para refletir a venda
+        }
+    },
+    actions: {
+        sellStock({commit}, order){
+            commit('sellStock', order) // chamando a mutation
+        },
+        getters: {
+            stockPortfolio(state, getters){ // vai retornar todas as ações que tenho
+                return state.stocks.map(stock => {
+                    const record = getters.stocks.find(element => element.id == stock.id) // precisa usar o getters para pegar do stocks/modules (é ele que tem todas as informações de uma ação)
+                    return { // ajustado para exibir todas as características de uma ação e não só o Id e a quantidade
+                        id: stock.id,
+                        quantity: stock.quantity,
+                        name: record.name, // nova informação
+                        price: record.price // nova informação
+                    }
+                })
+            },
+            funds(state){ // obter o estado do saldo, o valor que o usuário possui
+                return state.funds
+            }
+        }
+    }
+}
